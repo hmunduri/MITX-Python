c711d3559d51a2f6e750b5564a4433f00cea9444
M <lmiguelvargasf@gmail.com>
2015-11-08 00:13:31 -0500

    Implement test 1 and 2 for hangman.is_word_guessed.

--- Problem Set 3/hangman/hangman_test.py
+++ Problem Set 3/hangman/hangman_test.py
@@ -4,7 +4,24 @@
 #
 
 from unittest import TestCase
+import hangman
+import hangman_solution
 
 
 class HangmanIsWordGuessed(TestCase):
-    pass
+    def setUp(self):
+        pass
+
+    def assertWithSolution(self, secret_word, letters_guessed):
+        self.assertEquals(hangman_solution.is_word_guessed(secret_word, letters_guessed),
+                          hangman.is_word_guessed(secret_word, letters_guessed))
+
+    def test1(self):
+        secret_word = "apple"
+        letters_guessed = ['a', 'e', 'i', 'k', 'p', 'r', 's']
+        self.assertWithSolution(secret_word, letters_guessed)
+
+    def test2(self):
+        secret_word = "durian"
+        letters_guessed = ['h', 'a', 'c', 'd', 'i', 'm', 'n', 'r', 't', 'u']
+        self.assertWithSolution(secret_word, letters_guessed)
