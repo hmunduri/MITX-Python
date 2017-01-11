4a696ccfc8658230b12e962fe133d132e2b38ea1
M <lmiguelvargasf@gmail.com>
2017-01-11 18:55:11 -0500

    chore: Update docstring

--- problem_set_01/p_02_counting_bobs.py
+++ problem_set_01/p_02_counting_bobs.py
@@ -1,8 +1,10 @@
 __author__ = 'm'
 
 """
-This program prints the number of times the string "bob" occurs in a string.
-Assume s is a string of lower case characters. For example, if string = 'azcbobobegghakl', then the program prints:
+Assume s is a string of lower case characters.
+
+Write a program that prints the number of times the string 'bob' occurs in s.
+For example, if s = 'azcbobobegghakl', then your program should print
 
 Number of times bob occurs is: 2
 """
