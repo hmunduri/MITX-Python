09294327a5c5da1a778ab8bae0c8eea09f598349
M <lmiguelvargasf@gmail.com>
2017-01-11 19:05:13 -0500

    feat: Solve problem using comprehension

--- problem_set_01/p_02_counting_bobs.py
+++ problem_set_01/p_02_counting_bobs.py
@@ -20,9 +20,17 @@ def count_bobs(s: str) -> int:
     return counter
 
 
+def count_bobs_comprehension(s: str) -> int:
+    bob = 'bob'
+    bob_len = len(bob)
+    return len([bob for i in range(len(s) - bob_len + 1)
+                if s[i:i + bob_len] == bob])
+
 def main():
     s = input('Enter a string: ')
     print('Number of times bob occurs is:', str(count_bobs(s)))
+    print('Number of times bob occurs is (using comprehension):',
+          str(count_bobs(s)))
 
 
 if __name__ == '__main__':
