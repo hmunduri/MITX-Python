0cd5bfd022b289668946f1b37c9a08c4bdaa416a
M <lmiguelvargasf@gmail.com>
2017-01-11 19:05:41 -0500

    chore: Update docstring
    Explain that two solutions are provided. One that uses a for loop and the other that uses a comprehension.

--- problem_set_01/p_02_counting_bobs.py
+++ problem_set_01/p_02_counting_bobs.py
@@ -7,6 +7,10 @@ Write a program that prints the number of times the string 'bob' occurs in s.
 For example, if s = 'azcbobobegghakl', then your program should print
 
 Number of times bob occurs is: 2
+
+Two solutions are provided. The former solution uses a for loop, while the
+latter one uses a Python feature called comprehension which is widely used by
+Pythonistas.
 """
 
 
