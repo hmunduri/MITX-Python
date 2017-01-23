c7e9772092ab1214c6f9bd1af91039a613c4c3f7
M <lmiguelvargasf@gmail.com>
2017-01-23 17:54:06 -0500

    refactor: Refactor whole file
    Change docstrings to fit less than 80 characters, add function annotations, change double quotation marks to single quotation marks.

--- problem_set_02/p_03_using_bisection_search.py
+++ problem_set_02/p_03_using_bisection_search.py
@@ -1,30 +1,45 @@
 __author__ = 'm'
 
 """
-This program uses bisection search to find out the smallest monthly payment such that
-we can pay off the entire balance within a year.
+This program uses bisection search to find out the smallest monthly payment
+such that we can pay off the entire balance within a year.
 """
 
-from paying_debt_off_in_a_year import compute_balance_after_a_year
+from p_02_paying_debt_off_in_a_year import compute_balance_after
 
 
-def compute_initial_lower_and_upper_bounds(balance, annual_interest_rate):
-    monthly_interest_rate = annual_interest_rate / 12.0
-    lower_bound = balance / 12.0
-    upper_bound = balance * (1 + monthly_interest_rate) ** 12 / 12.0
+def compute_bounds(balance: float,
+                   annual_interest_rate: float) -> (float, float):
+    """"
+    :param balance: balance
+    :param annual_interest_rate: annual interest rate
+    :return: lower and upper bounds
+    """
+    monthly_interest_rate = annual_interest_rate / 12
+    lower_bound = balance / 12
+    upper_bound = balance * (1 + monthly_interest_rate) ** 12 / 12
 
     return lower_bound, upper_bound
 
 
-def compute_lowest_payment_using_binary_search(balance, annual_interest_rate):
-    lower_bound, upper_bound = compute_initial_lower_and_upper_bounds(balance, annual_interest_rate)
+def compute_lowest_payment(balance: float,
+                           annual_interest_rate: float) -> float:
+    """
+    :param balance: balance
+    :param annual_interest_rate: annual interest rate
+    :return: the lowest payment to pay balance within 12 months using
+    bisection search and rounded to 2 decimal places
+    """
+    lower_bound, upper_bound = compute_bounds(balance, annual_interest_rate)
 
     while True:
-        lowest_payment = (lower_bound + upper_bound) / 2.0
-        balance_after_a_year = compute_balance_after_a_year(balance, lowest_payment, annual_interest_rate)
+        lowest_payment = (lower_bound + upper_bound) / 2
+        balance_after_a_year = compute_balance_after(balance,
+                                                     lowest_payment,
+                                                     annual_interest_rate)
 
         if abs(balance_after_a_year) <= 0.01:
-            return lowest_payment
+            return round(lowest_payment, 2)
 
         if balance_after_a_year > 0:
             lower_bound = lowest_payment
@@ -33,12 +48,12 @@ def compute_lowest_payment_using_binary_search(balance, annual_interest_rate):
 
 
 def main():
-    balance = eval(input("Enter the initial balance: "))
-    annual_interest_rate = eval(input("Enter the annual interest rate: "))
+    balance = eval(input('Enter the initial balance: '))
+    annual_interest_rate = eval(input('Enter the annual interest rate: '))
 
-    lowest_payment = compute_lowest_payment_using_binary_search(balance, annual_interest_rate)
-    print("Lowest Payment: " + str(round(lowest_payment, 2)))
+    lowest_payment = compute_lowest_payment(balance, annual_interest_rate)
+    print('Lowest Payment: ' + str(lowest_payment))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
