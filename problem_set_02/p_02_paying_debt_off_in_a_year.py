2b424f198ced23a0c43777f92a257606e310c043
M <lmiguelvargasf@gmail.com>
2017-01-23 16:15:11 -0500

    chore: Change docstring

--- problem_set_02/p_02_paying_debt_off_in_a_year.py
+++ problem_set_02/p_02_paying_debt_off_in_a_year.py
@@ -1,8 +1,23 @@
 __author__ = 'm'
 
 """
-This program calculates the minimum fixed monthly payment needed in order to
-pay off a credit card balance within 12 months.
+Now write a program that calculates the minimum fixed monthly payment needed in
+order pay off a credit card balance within 12 months. By a fixed monthly
+payment, we mean a single number which does not change each month, but instead
+is a constant amount that will be paid each month.
+
+In this problem, we will not be dealing with a minimum monthly payment rate.
+
+The following variables contain values as described below:
+
+balance - the outstanding balance on the credit card
+
+annualInterestRate - annual interest rate as a decimal
+
+The program should print out one line: the lowest monthly payment that will
+pay off all debt in under 1 year, for example:
+
+Lowest Payment: 180
 """
 
 
