9123cac77dcb842d67316e40b7e688658b26260d
M <lmiguelvargasf@gmail.com>
2017-02-11 03:03:17 -0500

    complete: Solve problem 8

---
+++ midterm/feb_2017/problem_8.py
@@ -0,0 +1,17 @@
+def applyF_filterG(L, f, g):
+    """
+    Assumes L is a list of integers
+    Assume functions f and g are defined for you.
+    f takes in an integer, applies a function, returns another integer
+    g takes in an integer, applies a Boolean function,
+        returns either True or False
+    Mutates L such that, for each element i originally in L, L contains
+        i if g(f(i)) returns True, and no other elements
+    Returns the largest element in the mutated L or -1 if the list is empty
+    """
+    L_copy = L[:]
+    for i, e in enumerate(L_copy):
+        if not g(f(e)):
+            L.remove(e)
+
+    return max(L) if L else -1
