e01535fcaca776e30858d050d7ae5da3179a5c6d
M <lmiguelvargasf@gmail.com>
2017-02-11 02:25:34 -0500

    refactor: Use comprehension instead of for loop

--- midterm/feb_2017/problem_5.py
+++ midterm/feb_2017/problem_5.py
@@ -3,8 +3,4 @@ def dotProduct(listA, listB):
     listA: a list of numbers
     listB: a list of numbers of the same length as listA
     '''
-    dot_product = 0
-    for a, b in zip(listA, listB):
-        dot_product += a * b
-
-    return dot_product
+    return sum(a * b for a, b in zip(listA, listB))
