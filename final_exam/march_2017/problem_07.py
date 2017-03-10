ad59339b66abbeae0aac576a2a22be9042a56f6f
M <lmiguelvargasf@gmail.com>
2017-03-10 18:49:56 -0500

    exam: Solve problem 7 using a comprehensions

--- final_exam/march_2017/problem_07.py
+++ final_exam/march_2017/problem_07.py
@@ -1,4 +1,4 @@
-def general_poly (L):
+def general_poly(L):
     """ L, a list of numbers (n0, n1, n2, ... nk)
     Returns a function, which when applied to a value x, returns the value
     n0 * x^k + n1 * x^(k-1) + ... nk * x^0 """
@@ -9,5 +9,16 @@ def general_poly (L):
             total += (e * base ** power)
 
         return total
+
+    return compute
+
+
+def general_poly_comprehension(L):
+    """ L, a list of numbers (n0, n1, n2, ... nk)
+    Returns a function, which when applied to a value x, returns the value
+    n0 * x^k + n1 * x^(k-1) + ... nk * x^0 """
+    def compute(base):
+        powers = list(range(len(L)))[::-1]
+        return sum(e * base ** power for power, e in zip(powers, L))
 
     return compute
