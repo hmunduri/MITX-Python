e20f6a00474123d89001236a90460165e90f4164
M <lmiguelvargasf@gmail.com>
2017-03-10 12:03:43 -0500

    refactor: Rename variable

--- final_exam/march_2017/problem_07.py
+++ final_exam/march_2017/problem_07.py
@@ -2,11 +2,11 @@ def general_poly (L):
     """ L, a list of numbers (n0, n1, n2, ... nk)
     Returns a function, which when applied to a value x, returns the value
     n0 * x^k + n1 * x^(k-1) + ... nk * x^0 """
-    def compute(b):
+    def compute(base):
         total = 0
         powers = list(range(len(L) - 1, -1, -1))
         for power, e in zip(powers, L):
-            total += (e * b ** power)
+            total += (e * base ** power)
 
         return total
 
