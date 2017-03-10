0a06677a7abd18164e93ddf245cc051101ed27f9
M <lmiguelvargasf@gmail.com>
2017-03-10 10:42:13 -0500

    exam: Solve problem 3

---
+++ final_exam/march_2017/problem_03.py
@@ -0,0 +1,25 @@
+trans = {'0':'ling', '1':'yi', '2':'er', '3':'san', '4': 'si',
+          '5':'wu', '6':'liu', '7':'qi', '8':'ba', '9':'jiu', '10': 'shi'}
+
+def convert_to_mandarin(us_num):
+    '''
+    us_num, a string representing a US number 0 to 99
+    returns the string mandarin representation of us_num
+    '''
+    us_num_int = int(us_num)
+
+    if us_num_int in range(11):
+        return trans[us_num]
+    elif us_num_int in range(11, 20):
+        return '{} {}'.format(trans['10'], trans[us_num[-1]])
+    elif us_num_int % 10 == 0:
+        return '{} {}'.format(trans[us_num[0]], trans['10'])
+    else:
+        return '{} {} {}'.format(
+            trans[us_num[0]], trans['10'], trans[us_num[-1]])
+
+
+
+print(convert_to_mandarin('36'))
+print(convert_to_mandarin('20'))
+print(convert_to_mandarin('16'))
