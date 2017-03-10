ea87438632e09573a96448115db1bc86dbfd94c9
M <lmiguelvargasf@gmail.com>
2017-03-10 11:53:21 -0500

    exam: Solve problem 6

---
+++ final_exam/march_2017/problem_06.py
@@ -0,0 +1,40 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def say(self, stuff):
+        return self.name + ' says: ' + stuff
+
+    def __str__(self):
+        return self.name
+
+
+class Lecturer(Person):
+    def lecture(self, stuff):
+        return 'I believe that ' + Person.say(self, stuff)
+
+
+class Professor(Lecturer):
+    def say(self, stuff):
+        return self.name + ' says: ' + self.lecture(stuff)
+
+
+class ArrogantProfessor(Professor):
+    def say(self, stuff):
+        return Person.say(self, self.lecture(stuff))
+
+    def lecture(self, stuff):
+        return 'It is obvious that ' + Person.say(self, stuff)
+
+
+e = Person('eric')
+le = Lecturer('eric')
+pe = Professor('eric')
+ae = ArrogantProfessor('eric')
+
+print(e.say('the sky is blue'))
+print(le.say('the sky is blue'))
+print(le.lecture('the sky is blue'))
+print(pe.say('the sky is blue'))
+print(ae.say('the sky is blue'))
+print(ae.lecture('the sky is blue'))
